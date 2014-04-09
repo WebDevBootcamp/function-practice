575a157dee88bea007701e4576993b7b2dc1c24a
Matt Kunze <matt.kunze@datasplice.com>
2014-04-09 16:28:46 -0600

    add more examples
    improve validation, add some basic styling

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 function createExample(name, details) {
   var body = $('body');
   $('<h1>').text(name).appendTo(body);
-  $('<blockquote>').text(details.description).appendTo(body);
-  $('<pre>').text(details.implementation.toString()).appendTo(body);
+  $('<blockquote>')
+    .html(details.description)
+    .appendTo(body);
 
-  var validationList = $('<ul>');
-  for(var index = 0; index < details.validation.length; index++) {
-    var test = details.validation[index];
-    var status;
-    var results;
-    try
-    {
-      results = details.implementation.call(null, test.input);
-      if(results === test.output) {
-        status = 'success';
-      }
-      else {
-        status = 'incorrect';
-      }
-    }
-    catch (error)
-    {
-      status = 'error';
-      results = error;
-    }
+  if(details.implementation) {
+    $('<pre>')
+      .addClass('function-body')
+      .text(details.implementation.toString())
+      .appendTo(body);
 
-    var formatted = status + ': ' + test.input + ' -> ' + results;
-    $('<li>').addClass(status)
-      .text(formatted)
-      .appendTo(validationList);
+    var validationList = $('<ul>');
+    for(var index = 0; index < details.validation.length; index++) {
+      var test = details.validation[index];
+      var status;
+      var results;
+      try
+      {
+        var args = _.isArray(test.input) ? test.input : [ test.input ];
+        results = details.implementation.apply(null, args);
+        if(_.isEqual(results, test.output)) {
+          status = 'success';
+        }
+        else {
+          status = 'incorrect';
+        }
+      }
+      catch (error)
+      {
+        status = 'error';
+        results = error;
+      }
+
+      var formatted = status + ': ' + JSON.stringify(test.input) + ' -> ' +
+        JSON.stringify(results);
+      if(status === 'incorrect') {
+        formatted += ' (expected: ' + JSON.stringify(test.output) + ')';
+      }
+      $('<li>').addClass(status)
+        .text(formatted)
+        .appendTo(validationList);
+    }
+    validationList.appendTo(body);
   }
-  validationList.appendTo(body);
 }
 
